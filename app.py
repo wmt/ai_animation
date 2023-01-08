087ba885d9395f151d7bed885d2bbb96cdf23b08
Bill Tomlinson <wmtomlinsonjr@gmail.com>
2023-01-07 16:42:59 -0800

    initial commit

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
+from google.cloud import texttospeech
 import os
-
+import sys, subprocess, string
 import openai
+import requests
 from flask import Flask, redirect, render_template, request, url_for
+import google.auth.transport.requests
+import random
 
 app = Flask(__name__)
 openai.api_key = os.getenv("OPENAI_API_KEY")
@@ -9,27 +13,193 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 
 @app.route("/", methods=("GET", "POST"))
 def index():
+
+
+
+
+
+
     if request.method == "POST":
-        animal = request.form["animal"]
-        response = openai.Completion.create(
-            model="text-davinci-002",
-            prompt=generate_prompt(animal),
-            temperature=0.6,
+        setting_text = request.form["setting_text"]
+        character1_text = request.form["character1_text"]
+        character2_text = request.form["character2_text"]
+        plot_text = request.form["plot_text"]
+        style = request.form["style_text"]
+#        print("Setting = "+setting_text+", character1 = "+character1_text+", character2 = "+character2_text+", plot = "+plot_text+", style = "+style)
+
+        setting_list = ["rain forest", "ocean", "beach", "desert", "stream", "a universe we haven't explored yet", "under a rock", "waterfall"]
+        character_list = ["Sally the Garden Salamander", "Tanya the Tarantula", "Edgar the European Glass Lizard", "Kauri the Kaka", "Pablo the Pseudomys", "a goldendoodle"]
+
+        plot_list = ["reduce poverty", "feed the hungry", "heal the sick", "teach each other something", "make a world that's equal for all", "help people have clean water", "invent new energy technology", "make better infrastructure for their community", "reduce climate change", "protect endangered species", "resolve a conflict", "find a million dogs", "dancing together"]
+        style_list = ["child's drawing", "Van Gogh", "magic marker drawing", "Toulouse-Latrec", "Colored pencil", "squirrel"]
+
+        if setting_text == "":
+            setting_text = random.choice(setting_list)
+        if character1_text == "":
+            character1_text = random.choice(character_list)
+        if character2_text == "":
+            character2_text = random.choice(character_list)
+        if plot_text == "":
+            plot_text = random.choice(plot_list)
+        if style == "":
+            style = random.choice(style_list)
+
+        story_text = openai.Completion.create(
+           model="text-davinci-002",
+           prompt=generate_prompt("Write a long three paragraph story about a "+character1_text+ " and a "+character2_text+". The plot is "+plot_text+" and it takes place in a "+setting_text+". Please include lots of detail about all aspects of the story."),
+           temperature=0.9,
+            max_tokens=1200
+       )
+        story_text = story_text.choices[0].text
+#        print("*********************\n"+story_text+"\n*********************")
+        generate_new_images = True
+        if generate_new_images:
+            #style = "a well done drawing by a girl"
+
+
+#            style = "sidewalk chalk art"
+#            style = "child's magic marker drawing"
+            #style = "hyperrealistic paintings like Toulouse-Lautrec"
+#            style = "an animated television show"
+            setting_image = openai.Image.create(
+                #            prompt="An animated background for a story that looks like "+setting_text.choices[0].text,
+                prompt="A background in the "+style+" style that looks like " + setting_text,
+                n=1,
+                size="1024x1024"
+            )
+            character1_image = openai.Image.create(
+                prompt="A white background with "+character1_text+" character centered in the "+style+" style. The character is facing to the left. The background behind the character is completely white. The entire character is in the center of the picture. The character fits completely in the picture with no overlap with the edges.",
+                #prompt="The background is completely white. In the center there is a character in the "+style+" style of a "+character1_text+" facing to the left. Do not allow gigantic characters such as anything that doesn't fit in the box. ",
+                n=1,
+                size="256x256"
+            )
+            character2_image = openai.Image.create(
+                # prompt="A wide shot of a single animated character in the "+style+" style of a "+character2_text+" facing to the right. The background behind the character is completely white. Nothing else besides the character is in the picture. The entire character is in the center of the picture. The character fits in the picture with no overlap with the edges. The characters are very small.",
+                prompt="A white background with "+character2_text+" character centered in the "+style+" style. The character is facing to the right. The background behind the character is completely white. The entire character is in the center of the picture. The character fits completely in the picture with no overlap with the edges.",
+                n=1,
+                size="256x256"
+            )
+#            setting_image = setting_image['data'][0]['url']
+    #         character1_image = character1_image['data'][0]['url']
+    #        character2_image = character2_image['data'][0]['url']
+
+
+    #        setting_image = "static/ocean.png"
+    #        character1_image = "static/minnow.png"
+    #        character1_image = "https://e7.pngegg.com/pngimages/552/1/png-clipart-dogs-dogs-thumbnail.png"
+    #        character2_image = "static/shark.png"
+
+            img_data = requests.get(setting_image['data'][0]['url']).content
+            with open('static/setting_image_downloaded.png', 'wb') as handler:
+                handler.write(img_data)
+
+            img_data = requests.get(character1_image['data'][0]['url']).content
+            with open('static/character1_image_downloaded.png', 'wb') as handler:
+                handler.write(img_data)
+
+
+            img_data = requests.get(character2_image['data'][0]['url']).content
+            with open('static/character2_image_downloaded.png', 'wb') as handler:
+                handler.write(img_data)
+
+            #subprocess.call('color =$(convert static/shark.png -format "%[pixel:p{0,0}]" info:-)',shell=True)
+
+            command = 'convert static/character1_image_downloaded.png -brightness-contrast 8x8 static/character1_image_brighter.png'
+            subprocess.call(command, shell=True)
+#            command = 'convert static/character1_image_brighter.png -transparent white static/character1_image_transparent.png'
+            command = 'convert static/character1_image_brighter.png -bordercolor white -border 1x1 -alpha set -channel RGBA -fuzz 3% -fill none -floodfill +0+0 white -morphology erode square: 1 -shave 1x1 static/character1_image_transparent.png'
+            subprocess.call(command, shell=True)
+
+            command = 'convert static/character2_image_downloaded.png -brightness-contrast 8x8  static/character2_image_brighter.png'
+            subprocess.call(command, shell=True)
+ #           command = 'convert static/character2_image_brighter.png -transparent white static/character2_image_transparent.png'
+            command = 'convert static/character2_image_brighter.png -bordercolor white -border 1x1 -alpha set -channel RGBA -fuzz 3% -fill none -floodfill +0+0 white -morphology erode square: 1 -shave 1x1 static/character2_image_transparent.png'
+            subprocess.call(command, shell=True)
+        else:
+            print("Using cached images")
+
+
+
+
+
+
+
+
+
+
+#        subprocess.call('rm static/character1_image_brighter.png')
+#        subprocess.call('rm static/character1_image_transparent.png')
+
+       # subprocess.call('convert shark.png - alpha off - bordercolor $color - border 1 \(+clone - fuzz 30 % -fill none -floodfill +0+0 $color -alpha extract -geometry 200 % -blur 0x0.5 -morphology erode square:1 -geometry 50 %  \)  - compose CopyOpacity - composite - shave 1 static/shark2.png', shell=True)
+
+        setting_image = "static/setting_image_downloaded.png"
+        character1_image_transparent = 'static/character1_image_transparent.png'
+        character2_image_transparent = 'static/character2_image_transparent.png'
+
+        # credentials, project_id = google.auth.default()
+        # credentials.refresh(google.auth.transport.requests.Request())
+        # print('project id:', project_id)
+        # print('id token:', credentials.id_token)
+        # response = requests.get('https://www.googleapis.com/oauth2/v1/tokeninfo?id_token=' + credentials.id_token)
+        # print('tokeninfo:')
+        # print(response.text)
+
+        """Synthesizes speech from the input string of text or ssml.
+        Make sure to be working in a virtual environment.
+
+        Note: ssml must be well-formed according to:
+            https://www.w3.org/TR/speech-synthesis/
+        """
+
+        # Instantiates a client
+        client = texttospeech.TextToSpeechClient()
+
+        # Set the text input to be synthesized
+        synthesis_input = texttospeech.SynthesisInput(text=story_text)
+
+        # Build the voice request, select the language code ("en-US") and the ssml
+        # voice gender ("neutral")
+        voice = texttospeech.VoiceSelectionParams(
+            #https://cloud.google.com/speech-to-text/docs/speech-to-text-supported-languages
+            language_code="en-GB", ssml_gender=texttospeech.SsmlVoiceGender.FEMALE
         )
-        return redirect(url_for("index", result=response.choices[0].text))
 
-    result = request.args.get("result")
-    return render_template("index.html", result=result)
+        # Select the type of audio file you want returned
+        audio_config = texttospeech.AudioConfig(
+            audio_encoding=texttospeech.AudioEncoding.MP3
+        )
+
+        # Perform the text-to-speech request on the text input with the selected
+        # voice parameters and audio file type
+        response = client.synthesize_speech(
+            input=synthesis_input, voice=voice, audio_config=audio_config
+        )
+
+        # The response's audio_content is binary.
+        with open("static/output.mp3", "wb") as out:
+            # Write the response to the output file.
+            out.write(response.audio_content)
+            print('Audio content written to file "output.mp3"')
 
 
-def generate_prompt(animal):
-    return """Suggest three names for an animal that is a superhero.
 
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: {}
-Names:""".format(
-        animal.capitalize()
+
+
+
+
+
+
+
+        return redirect(url_for("index", setting_text=setting_text, setting_image=setting_image, character1_image=character1_image_transparent, character2_image=character2_image_transparent))
+
+    setting_text = request.args.get("setting_text")
+    setting_image = request.args.get("setting_image")
+    character1_image = request.args.get("character1_image")
+    character2_image = request.args.get("character2_image")
+    return render_template("index.html", setting_text=setting_text, setting_image=setting_image, character1_image=character1_image, character2_image=character2_image)
+
+
+def generate_prompt(setting_text):
+    return """Write a two sentence description of the following setting: {}""".format(
+        setting_text.capitalize()
     )
---
+++ google-cloud-sdk/lib/googlecloudsdk/command_lib/artifacts/cleanup_policy_util.py
@@ -0,0 +1,103 @@
+# -*- coding: utf-8 -*- #
+# Copyright 2022 Google LLC. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Utility for forming Artifact Registry requests around cleanup policies."""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import unicode_literals
+
+import json
+
+from apitools.base.py import exceptions as apitools_exceptions
+from googlecloudsdk.api_lib.artifacts import exceptions as ar_exceptions
+from googlecloudsdk.core.console import console_io
+from googlecloudsdk.core.util import encoding
+from googlecloudsdk.core.util import times
+import six
+
+
+def ParseCleanupPolicy(path):
+  """Reads a cleanup policy from a JSON formatted file.
+
+  Args:
+    path: str, path to the policy file.
+
+  Returns:
+    A dict describing a cleanup policy, matching the proto description.
+
+  Raises:
+    InvalidInputValueError: The JSON file could not be parsed or the data does
+    not follow the correct schema.
+  """
+  content = console_io.ReadFromFileOrStdin(path, binary=False)
+  try:
+    file_policies = json.loads(encoding.Decode(content))
+  except ValueError as e:
+    raise apitools_exceptions.InvalidUserInputError(
+        'Could not read JSON file {}: {}'.format(path, e))
+  policies = dict()
+  for policy in file_policies:
+    for key in ['name', 'action', 'condition']:
+      if key not in policy:
+        raise ar_exceptions.InvalidInputValueError(
+            'Key "{}" not found in policy.'.format(key))
+    if 'type' not in policy['action']:
+      raise ar_exceptions.InvalidInputValueError(
+          'Key "type" not found in policy action.')
+    condition = dict()
+    if 'versionAge' in policy['condition']:
+      seconds = times.ParseDuration(policy['condition']['versionAge'])
+      condition['versionAge'] = six.text_type(seconds.total_seconds) + 's'
+    policies[policy['name']] = {
+        'id': policy['name'],
+        'condition': condition,
+        'action': policy['action']['type'],
+    }
+  return policies
+
+
+def SetDeleteCleanupPolicyUpdateMask(unused_ref, unused_args, request):
+  """Sets update mask for deleting Cleanup Policies."""
+  request.updateMask = 'cleanup_policies'
+  return request
+
+
+def RepositoryToCleanupPoliciesResponse(response, unused_args):
+  if not response.cleanupPolicies:
+    return None
+  return response.cleanupPolicies.additionalProperties
+
+
+def RepositoryToSetCleanupPolicyResponse(response, unused_args):
+  return response.cleanupPolicies.additionalProperties
+
+
+def SetOverwriteMask(unused_ref, args, request):
+  if args.overwrite:
+    request.updateMask = None
+  else:
+    request.updateMask = 'cleanup_policies'
+  return request
+
+
+def DeleteCleanupPolicyFields(unused_ref, args, request):
+  removed_policies = args.policynames.split(',')
+  remaining_policies = []
+  if request.repository.cleanupPolicies:
+    for policy in request.repository.cleanupPolicies.additionalProperties:
+      if policy.key not in removed_policies:
+        remaining_policies.append(policy)
+  request.repository.cleanupPolicies.additionalProperties = remaining_policies
+  request.updateMask = None
+  return request
